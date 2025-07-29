b1dbd16792a24891276c77491fd1396e4c8443ed
Luke Goule <luke.goule@gmail.com>
2025-07-29 18:43:17 +0100

    Showing session errors to user for task name field.

--- resources/views/components/create-task-form.blade.php
+++ resources/views/components/create-task-form.blade.php
@@ -1,7 +1,10 @@
 <form action="{{ route('task.store') }}" method="POST">
     <div class="d-flex flex-column gap-3">
         <div class="form-group">
-            <input type="text" name="name" placeholder="Insert task name" class="form-control" />
+            <input type="text" name="name" placeholder="Insert task name" class="form-control" value="{{ old('name') }}" />
+            @error('name')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <input type="submit" class="btn btn-primary" />
